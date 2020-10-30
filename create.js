a7a1c3694ff5b629f424dc1c6f1ea9b8a8a462af
Pawan Sah <sa3pawan@gmail.com>
2020-10-31 01:27:14 +0530

    refactoring  and creating handler and dynamodb lib 1

--- create.js
+++ create.js
@@ -1,11 +1,8 @@
 import * as uuid from 'uuid';
-import AWS from 'aws-sdk';
+import handler from './libs/handler-lib'
+import dynamoDb from './libs/dynamodb-lib'
 
-// AWS.config.update({ region: "" })
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-
-export function main(event, context, callback) {
+export const main = handler(async (event, context) => {
     // Request body is passed in as a JSON encoded string in 'event.body'
     const data = JSON.parse(event.body);
 
@@ -21,31 +18,6 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamoDb.put(params, (error, data) => {
-        // set response headers to enable cors
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-
-        // Return status code 500 on error
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-            callback(null, response);
-            return;
-        }
-
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });
-
-}
+    await dynamoDb.put(params);
+    return params.Item;
+});
